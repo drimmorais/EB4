b62c862d362f8b6b7baa63bf71c6a79bc315a809
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:38:39 -0300

    finzinho

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     int i = 21, k = 18, j = 72, cont = 0;
 
-    while(cont <= 5){
+    while(cont <= 20){
       System.out.println(i);  
       cont ++;  
     }
@@ -12,9 +12,9 @@ class Main {
    do{
      System.out.println(k);
      cont ++;
-   }while(cont <= 5);
+   }while(cont <= 20);
 
-   for (i = 0; i <= 5; i++){
+   for (i = 0; i <= 20; i++){
      System.out.println(j);
    }
 
