bad733cfcd05f3b2bcb68d90c62d2be49d94eddc
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:38:21 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    int i = 21, k = 18, j = 72, cont = 0;
+
+    while(cont <= 5){
+      System.out.println(i);  
+      cont ++;  
+    }
+
+   cont = 0;
+
+   do{
+     System.out.println(k);
+     cont ++;
+   }while(cont <= 5);
+
+   for (i = 0; i <= 5; i++){
+     System.out.println(j);
+   }
+
+  }
+}
